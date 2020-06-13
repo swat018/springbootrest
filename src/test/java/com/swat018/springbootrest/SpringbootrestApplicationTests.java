6eab58d9253e925ee669195eb40d9414befb4b9a
jinwoo park <swat018@gmail.com>
2020-06-13 13:14:27 +0900

    Initial commit

---
+++ src/test/java/com/swat018/springbootrest/SpringbootrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swat018.springbootrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootrestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
